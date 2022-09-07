7ce1512079862e0142783044728ccb5ef34d4274
Yevhen Synytsia <yevhensynytsia@Yevhens-MacBook-Pro.local>
2022-09-07 15:32:58 +0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 5000);
 }
-bootstrap()
+bootstrap();
